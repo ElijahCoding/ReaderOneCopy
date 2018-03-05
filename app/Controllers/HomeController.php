b72fb684195c36bba4c0e1095e705ba7d40a6b49
Elijah <elijahsharper1@gmail.com>
2018-03-06 00:02:02 +0300

    Set up cache adapter

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -12,6 +12,8 @@ class HomeController extends Controller
 {
     public function index(Request $request, Response $response, $args)
     {
+        dump($this->c->cache);
+        die();
         return $this->c->view->render($response, 'home/index.twig', [
             'appName' => $this->c->settings['app']['name'],
         ]);
